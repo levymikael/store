62fbd95266b4f89f2ae980bf9c569493adba8e4c
levymikael <51111456+levymikael@users.noreply.github.com>
2019-07-21 20:07:40 +0300

    done

--- store.py
+++ store.py
@@ -2,6 +2,16 @@ from bottle import route, run, template, static_file, get, post, delete, request
 from sys import argv
 import json
 import pymysql
+import store
+
+connection = pymysql.connect(host='localhost',
+                     user='root',
+                     password='root',
+                     db='store',
+                     charset='utf8',
+                     cursorclass=pymysql.cursors.DictCursor)
+
+
 
 @get("/admin")
 def admin_portal():
@@ -29,4 +39,57 @@ def images(filename):
     return static_file(filename, root='images')
 
 
-run(host='0.0.0.0', port=argv[1])
+@post('/category')
+def add_category():
+    category_name = request.forms.get("name")
+    with connection.cursor() as cursor:
+            sql = "select category from category WHERE category = '{}'".format(category_name)
+            cursor.execute(sql)
+            result= cursor.fetchone()
+            try:
+                if result['category'] == category_name:
+                    return json.dumps({"STATUS": "fail", "MSG": "", "CODE": 404})
+            except:
+
+                adding_category = 'INSERT INTO category (category) VALUES( % s) '
+                val = (category_name)
+                cursor.execute(adding_category, val)
+                connection.commit()
+                return json.dumps(category_name)
+
+
+
+@get('/categories')
+def show_category():
+    with connection.cursor() as cursor:
+        sql = "SELECT category FROM category"
+        cursor.execute(sql)
+        result = cursor.fetchall( )
+
+    return json.dumps(result)
+
+
+@delete('/categories/<id>')
+def delete_category(id):
+    with connection.cursor() as cursor:
+        sql = "select cat_id from category WHERE cat_id = {}".format(id)
+        cursor.execute(sql)
+
+        result = cursor.fetchone()
+
+
+        try:
+            if int(id) == int(result['cat_id']):
+                sql_t = "DELETE from category WHERE cat_id = {}".format(id)
+                cursor.execute(sql_t)
+                connection.commit()
+                result = cursor.fetchone()
+
+                return json.dumps({"STATUS": "success", "MSG": "category deleted successfully", "CODE": 201})
+        except:
+
+            return json.dumps({"STATUS": "fail", "MSG": "Category not found", "CODE": 404})
+
+
+run(host='localhost', port=7040)
+
